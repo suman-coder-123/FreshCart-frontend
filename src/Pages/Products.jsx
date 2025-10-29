6dc3332b6a2f6d78354ab9cc6791cdf85adb2db1
suman-coder-123 <sumanpatidar2006@gmail.com>
2025-10-29 22:26:43 +0530

    dfs

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -27,7 +27,7 @@ export default function Products() {
       <h1 className="text-[#21313c] text-2xl font-medium my-10">
         Popular Products
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
         {product.map((item) => {
           return (
             <>
